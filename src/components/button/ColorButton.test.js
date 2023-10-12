14bdc5ed82cc29695063e9be4edda2f7dd930921
louknut <thanutda78@gmail.com>
2023-10-12 21:26:30 +0700

    Color button test pass

--- src/components/button/ColorButton.test.js
+++ src/components/button/ColorButton.test.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "@testing-library/jest-dom"
-import {  } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import ColorButton from "./ColorButton"
+
+beforeEach(() => render(<ColorButton/>))
 
 test("initial button color", () => {
-
+    // render(<ColorButton/>);
+    const button = screen.getByRole("button", { name: /change to blue/i})
+    // const button = screen.getByRole("button")
+    // screen.debug(button)
+    expect(button).toHaveStyle({backgroundColor: "red"});
+    expect(button).toHaveClass("ui button");
 })
 
-test("initial button color xx", () => {
-
+test("click button change from red to blue", () => {
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(button).toHaveStyle({backgroundColor: "blue"});
+    expect(button).toHaveTextContent("Change to red");
 })
